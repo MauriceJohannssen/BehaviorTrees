59c453140c916180711e147c0d17d60537e2e72c
Maurice Johannssen <60900273+RulesOfDelta@users.noreply.github.com>
2021-02-03 10:20:24 +0100

    Min distance to boss

--- PersonalPortfolio/Assets/Scripts/AI/BehaviorNodes/Patrol/PatrolNode.cs
+++ PersonalPortfolio/Assets/Scripts/AI/BehaviorNodes/Patrol/PatrolNode.cs
@@ -1,36 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PatrolNode : Node
 {
     private AIBlackboard _AI;
     private Vector3 _nextOffset;
-    private float _remainAtPositionTime = 2.0f;
-    
+    private Vector3 _currentPosition;
+    private float _timeToGoToPosition;
+    private float _currentTime;
+    private Vector3 _minimumOffset = Vector3.one * 5;  
+
     public PatrolNode(AIBlackboard AI)
     {
         _AI = AI;
-        _nextOffset = new Vector3(Random.Range(-1.0f,1.0f), 0, Random.Range(-1.0f, 1.0f)) * 10;
-        //_AI.NavAgent.SetDestination(_AI.GetBoss().transform.position + _nextOffset);
+        _currentPosition = _AI.transform.position;
+        _nextOffset = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f)) * 10;
+        _timeToGoToPosition = Random.Range(3.0f, 5.0f);
+        _currentTime = 0;
     }
 
     public override State EvaluateState()
     {
         _AI.NavAgent.isStopped = false;
         
-        if (_AI.NavAgent.remainingDistance <= 1.0f)
+        if (_currentTime > _timeToGoToPosition)
         {
-            if (_remainAtPositionTime <= 0)
-            {
-                _nextOffset = new Vector3(Random.Range(-1.0f,1.0f), 0, Random.Range(-1.0f, 1.0f)) * 10;
-                _remainAtPositionTime = Random.Range(0.5f, 2.0f);
-            }
-
-            _remainAtPositionTime -= Time.deltaTime;
+            _currentPosition = _nextOffset;
+            if(Random.Range(0,2) == 0) _nextOffset = _minimumOffset + new Vector3(Random.Range(0.0f, 1.0f), 0, Random.Range(0.0f, 1.0f)) * 8;
+            else _nextOffset = -_minimumOffset + new Vector3(Random.Range(-1.0f, 0.0f), 0, Random.Range(-1.0f, 0.0f)) * 8;
+            _timeToGoToPosition = Random.Range(3.0f, 5.0f);
+            _currentTime = 0;
         }
         
-        _AI.NavAgent.SetDestination(_AI.GetBoss().transform.position + _nextOffset);
+        _currentTime += Time.deltaTime;
+
+        Vector3 interpolatedPosition = Vector3.Lerp(_currentPosition, _nextOffset, _currentTime / _timeToGoToPosition);
+
+        _AI.NavAgent.SetDestination(_AI.GetBoss().transform.position + interpolatedPosition);
         return State.Success;
     }
 }
